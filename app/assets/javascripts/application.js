2364ae4dba95a2be12e6d511307bdd576eef8a0e
Efrain Ayllon <jesusayllonroa@gmail.com>
2014-11-14 08:21:39 -0600

    Map is working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(window).ready(function(){
+
+// var map = L.map('map').setView([51.505, -0.09], 13);
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+// var popup = L.popup();
+//
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
+//
+// map.on('click', onMapClick);
+
+
+
+  window.map = L.map('map').setView([19.2944337,-99.6397071], 16);
+
+
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+  	maxZoom: 20,
+  	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  	id: 'examples.map-i875mjb7'
+  }).addTo(window.map);
+
+var marker = L.marker([19.293500,-99.6397071]).addTo(map);
+
+var popup = L.popup()
+    .setLatLng([19.2944337,-99.6397071], 16)
+    .setContent("<b>Hello world!</b><br>Efrain was born here, in <b>Toluca, Mexico!</b>")
+    .openOn(map);
+});
+
+
+var popup = L.popup();
